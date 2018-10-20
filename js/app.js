9e3fc2c645668becf4df6dea640066aabf6e99d6
Skyler Burger <skylerburger@gmail.com>
2018-10-20 16:24:49 -0700

    completed catalog.js TODOs

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ Cart.prototype.addItem = function(product, quantity) {
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
-  localStorage.setItem('unfinishedCart', JSON.stringify(cart.items));
+  localStorage.setItem('cart', JSON.stringify(cart.items));
 };
 
 Cart.prototype.removeItem = function(itemIndex) {
